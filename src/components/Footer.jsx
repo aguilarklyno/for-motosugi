d92fbd107acfeee2dde5bf3bd41dace8848e977d
klyno aguilar <klynoaguilar@klynonoMacBook-Air.local>
2022-11-26 15:06:44 +0900

    Footer responsive done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,44 +7,44 @@ import {HiDesktopComputer} from "react-icons/hi";
 const Footer = () => {
   return (
     <div>
-      <div className='flex justify-around items-center w-full h-auto bg-white'>
-        <ul className='p-6 w-auto'>
+      <div className='flex md:flex-row flex-col justify-around items-center w-full h-auto bg-white'>
+        <ul className='p-6 my-2 w-auto'>
          
-          <li><Link to="/service" className='2xl:text-base  xl:text-sm text-xs xl:mr-2 mx-0'>POSサービス・機能</Link></li>
+          <li><Link to="/service" className='text-lg'>POSサービス・機能</Link></li>
           <AiFillCaretRight className='mb-4' size={10}/>
-          <li><Link to="/cashless" className='2xl:text-base  xl:text-sm text-xs xl:mr-2 mx-0'>キャッシュレス対応</Link></li>
+          <li><Link to="/cashless" className='text-lg'>キャッシュレス対応</Link></li>
           <AiFillCaretRight className='mb-4' size={10}/>
-          <li><Link to="/custom" className='2xl:text-base  xl:text-sm text-xs xl:mr-2 mx-0'>カスタマイズ対応</Link></li>
+          <li><Link to="/custom" className='text-lg'>カスタマイズ対応</Link></li>
           <AiFillCaretRight className='mb-4' size={10}/>
-          <li><Link to="/plan" className='2xl:text-base  xl:text-sm text-xs xl:mr-2 mx-0'>料金プラン</Link></li>
+          <li><Link to="/plan" className='text-lg'>料金プラン</Link></li>
           <AiFillCaretRight className='mb-4' size={10}/>
-          <li><Link to="/simulation" className='2xl:text-base  xl:text-sm text-xs xl:mr-2 mx-0'>お見積りシュミレーション</Link></li>
+          <li><Link to="/simulation" className='text-lg'>お見積りシュミレーション</Link></li>
           <AiFillCaretRight className='mb-4' size={10}/>
-          <li><Link to="/colum" className='2xl:text-base  xl:text-sm text-xs xl:mr-2 mx-0'>コラム</Link></li>
+          <li><Link to="/colum" className='text-lg'>コラム</Link></li>
           <AiFillCaretRight className='mb-4' size={10}/>
-          <li><Link to="/news" className='2xl:text-base  xl:text-sm text-xs xl:mr-2 mx-0'>ニュース</Link></li>
+          <li><Link to="/news" className='text-lg'>ニュース</Link></li>
           <AiFillCaretRight className='mb-4' size={10}/>
-          <li><Link to="/faq" className='2xl:text-base  xl:text-sm text-xs xl:mr-2 mx-0'>FAQ</Link></li>
+          <li><Link to="/faq" className='text-lg'>FAQ</Link></li>
           <AiFillCaretRight className='mb-4' size={10}/>
         </ul>
 
         <div className='flex flex-col items-center justify-center'>
-          <div className='flex justify-center bg-gray-300 h-56 w-96 py-10 rounded-lg'>
-            <div className='flex flex-col h-36 items-center border-r-2 p-4'>
+          <div className='flex md:flex-row flex-col justify-center bg-gray-300 h-auto w-auto py-12 rounded-lg'>
+            <div className='flex flex-col h-36 items-center md:border-r-2 md:border-b-0 border-b-2 px-4  md:mx-0 mx-32'>
               <h1 className='text-xs '>オンライン商談</h1>
               <HiDesktopComputer className='my-2' size={60}/>
               <div className='bg-yellow-300 text-center rounded-full'>
                 <p className='p-2 m-0 text-xs'>お申し込み</p>
               </div>
             </div>
-            <div className='flex flex-col h-36 items-center border-r-2 p-4'>
+            <div className='flex flex-col h-36 items-center md:border-r-2 md:border-b-0 border-b-2 px-4 md:mt-0 mt-4 md:mx-0 mx-32'>
               <h1 className='text-xs '>オンライン商談</h1>
               <HiDesktopComputer className='my-2' size={60}/>
               <div className='bg-yellow-300 text-center rounded-full'>
                 <p className='p-2 m-0 text-xs'>お申し込み</p>
               </div>
             </div>
-            <div className='flex flex-col h-36 items-center p-4'>
+            <div className='flex flex-col h-36 items-center px-4 md:mt-0 mt-4 md:mx-0 mx-32'>
               <h1 className='text-xs '>オンライン商談</h1>
               <HiDesktopComputer className='my-2' size={60}/>
               <div className='bg-yellow-300 text-center rounded-full'>
